4c00d34e41b521cd3eb0286cd988ae0ddf9aae8f
zx-zee04 <mnrdhan04@gmail.com>
2025-09-26 23:07:34 +0700

    Tes kode

--- posttest2/lib/main.dart
+++ posttest2/lib/main.dart
@@ -7,116 +7,302 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Katalog Resep',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        brightness: Brightness.light,
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.black,
+          foregroundColor: Colors.white,
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+// page 1 / home 
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  void showComingSoon(BuildContext context, String fitur) {
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Text("Fitur $fitur"),
+        content: const Text("Coming Soon 🚧"),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(ctx),
+            child: const Text("OK"),
+          ),
+        ],
+      ),
+    );
+  }
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Widget resepItem(BuildContext context, String judul, String kategori,
+      String rating, String deskripsi) {
+    return Container(
+      width: 320,
+      margin: const EdgeInsets.all(12),
+      padding: const EdgeInsets.all(14),
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        border: Border.all(color: Colors.grey[700]!, width: 2),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // tempat gambar
+          Container(
+            height: 120,
+            color: Colors.grey[400],
+          ),
+          const SizedBox(height: 8),
+          Text(
+            judul,
+            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 6),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("Kategori: $kategori"),
+              Text(rating),
+            ],
+          ),
+          const SizedBox(height: 8),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => DetailPage(
+                    judul: judul,
+                    kategori: kategori,
+                    rating: rating,
+                    deskripsi: deskripsi,
+                  ),
+                ),
+              );
+            },
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              alignment: Alignment.center,
+              color: Colors.grey[800],
+              child: const Text(
+                "Lihat Detail",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text("Katalog Resep"),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () => showComingSoon(context, "Cari Resep"),
+            icon: const Icon(Icons.search),
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SingleChildScrollView(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          children: [
+            //fitur
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      IconButton(
+                        onPressed: () => showComingSoon(context, "Tambah Resep"),
+                        icon: const Icon(Icons.add_circle, size: 40),
+                        color: Colors.grey[800],
+                      ),
+                      const Text("Tambah Resep"),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      IconButton(
+                        onPressed: () =>
+                            showComingSoon(context, "Lihat Semua Resep"),
+                        icon: const Icon(Icons.list_alt, size: 40),
+                        color: Colors.grey[800],
+                      ),
+                      const Text("Lihat Semua"),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      IconButton(
+                        onPressed: () =>
+                            showComingSoon(context, "Edit/Hapus Resep"),
+                        icon: const Icon(Icons.edit, size: 40),
+                        color: Colors.grey[800],
+                      ),
+                      const Text("Edit Resep"),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+            //resep
+            resepItem(
+              context,
+              "Sate Ayam Madura",
+              "Makanan Utama",
+              "⭐ 4.7",
+              "Daging Kecil ditusuk, lalu dibakar dan diberi kacang oleh madura",
+            ),
+            resepItem(
+              context,
+              "Es Campur Segar",
+              "Minuman",
+              "⭐ 4.5",
+              "Es dicampur dengan kesegaran murni dari tembakau pilihan",
+            ),
+            resepItem(
+              context,
+              "Nasi Goreng Spesial",
+              "Makanan Utama",
+              "⭐ 4.8",
+              "Makanan sejuta umat yang gampang dibikin",
+            ),
+            resepItem(
+              context,
+              "Pisang Goreng Crispy",
+              "Dessert",
+              "⭐ 4.6",
+              "Hihang khoreng nama aslinya",
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+//Page2 detail Makanan
+class DetailPage extends StatelessWidget {
+  final String judul;
+  final String kategori;
+  final String rating;
+  final String deskripsi;
+
+  const DetailPage({
+    super.key,
+    required this.judul,
+    required this.kategori,
+    required this.rating,
+    required this.deskripsi,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(judul),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Container(
+            width: 320,
+            margin: const EdgeInsets.all(16),
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.grey[100],
+              border: Border.all(color: Colors.grey[700]!, width: 2),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // 3 gambar
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      width: 90,
+                      height: 90,
+                      color: Colors.grey[400],
+                      child: const Center(
+                        child: Text("1", style: TextStyle(color: Colors.white)),
+                      ),
+                    ),
+                    Container(
+                      width: 90,
+                      height: 90,
+                      color: Colors.grey[500],
+                      child: const Center(
+                        child: Text("2", style: TextStyle(color: Colors.white)),
+                      ),
+                    ),
+                    Container(
+                      width: 90,
+                      height: 90,
+                      color: Colors.grey[600],
+                      child: const Center(
+                        child: Text("3", style: TextStyle(color: Colors.white)),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  judul,
+                  style:
+                      const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Kategori: $kategori"),
+                    Text(rating),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Text("Deskripsi: $deskripsi"),
+                const SizedBox(height: 20),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(10),
+                    alignment: Alignment.center,
+                    color: Colors.grey[800],
+                    child: const Text(
+                      "Kembali",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
